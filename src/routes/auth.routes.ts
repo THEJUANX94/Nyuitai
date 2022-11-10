fc4502566e92a9a05e6ae2c54240433745d8e86b
THEJUANX94 <sebastianmn03@hotmail.com>
2022-11-10 14:46:23 -0500

    .

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
-const router :Router = Router();
+const router: Router = Router();
 
-import {signup, signin, profile} from '../controllers/auth.controller'
+import { signup, signin, profile } from '../controllers/auth.controller'
 import { TokenValidation } from "../libs/verifyToken";
 
 router.post('/signup', signup);
-router.post('/signin', signin) ;
+router.post('/signin', signin);
 
 router.get('/profile', TokenValidation, profile);
 
